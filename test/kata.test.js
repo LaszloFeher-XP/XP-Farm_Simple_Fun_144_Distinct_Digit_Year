72cf6b44e73414d83c70177673e4eefa85adbdc3
Laszlo Feher <Laszlo.Feher@ibm.com>
2022-04-13 10:26:54 +0200

    feat: 2nd, red

--- test/kata.test.js
+++ test/kata.test.js
@@ -1,9 +1,14 @@
 const { Kata } = require('../src/kata');
 
 describe('First story', () => {
-  test('GIVEN ... WHEN ... THEN ...', () => {
+  test('GIVEN 2013 WHEN logic THEN should return 2014', () => {
     const kata = new Kata();
     expect(kata.distinctDigitYear(2013)).toStrictEqual(2014);
+  });
+
+  test('GIVEN 2019 WHEN logic THEN should return 2031', () => {
+    const kata = new Kata();
+    expect(kata.distinctDigitYear(2019)).toStrictEqual(2031);
   });
 });
 
